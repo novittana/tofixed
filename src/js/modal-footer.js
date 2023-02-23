055131a2e53d78bf367cb095e2e0748bf8650747
Tetiana Novitska <novittana2410@gmail.com>
2023-02-23 23:40:48 +0200

    init

--- src/js/modal-footer.js
+++ src/js/modal-footer.js
@@ -4,13 +4,13 @@ const refs = {
     footerModalEl: document.querySelector('[data__footer-modal]'),
   };
 
-
  refs.footerModalOpenEl.addEventListener('click', toggleModal);
     refs.footerCloseBtnEl.addEventListener('click', toggleModal);
   
     function toggleModal() {
        refs. footerModalEl.classList.toggle('is-hidden');
     }
+
 
      document.addEventListener('keydown', function(e) {
        if (e.key === 'Escape') {
